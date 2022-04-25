73331240869acf9f6b385412fca9479f86650a1c
indhiranis <99375735+indhiranis@users.noreply.github.com>
2022-04-25 19:35:57 +0530

    Delete this.js

--- this.js
+++
@@ -1,8 +0,0 @@
-function display(name,age){
-    this.name="jk";
-    this.age=25;
-}
-const person=new display();
-display.prototype.gender="male"//syntax to add an object constructor function (objectConstructorName.prototype.key="value";)
-console.log(person.prototype);
-console.log(person.gender);
